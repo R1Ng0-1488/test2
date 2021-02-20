8461bcf19bd35bc52b097fb86f26033aa5c9ed0e
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-02-20 13:09:10 +0500

    Based view returns minamal HTML now

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
 
 from lists.views import home_page
 # Create your tests here.
@@ -12,3 +13,12 @@ class HomePageTest(TestCase):
 		'''тест: корневой url преобразуется в представление домашней страницы'''
 		found = resolve('/') # через url находит функцию представления
 		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_correct_html(self):
+		'''тест: домашняя страница возвращает правильный html'''
+		request = HttpRequest()
+		response = home_page(request)
+		html = response.content.decode('utf-8')
+		self.assertTrue(html.startswith('<html>'))
+		self.assertIn('<title>To-Do lists</title>', html)
+		self.assertTrue(html.endswith('</html>'))
