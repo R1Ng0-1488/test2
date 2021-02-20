22a2d1b03e3acd42f533e84e957d43e2bd5d1290
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-02-20 12:52:41 +0500

    THe first modul test and  changed url

--- lists/tests.py
+++ lists/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
 
+from lists.views import home_page
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
-	'''toxic test'''
+class HomePageTest(TestCase):
+	'''Тест домашней страницы'''
 
-	def test_bad_maths(self):
-		'''тест'''
-
-		self.assertEqual(1+1, 3)
+	def test_root_url_resolves_to_home_page_view(self):
+		'''тест: корневой url преобразуется в представление домашней страницы'''
+		found = resolve('/') # через url находит функцию представления
+		self.assertEqual(found.func, home_page)
