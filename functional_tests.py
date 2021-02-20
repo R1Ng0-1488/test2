83772042a09850e7fce8506805b0cbec1d89c19e
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-02-20 12:13:34 +0500

    Changed functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,47 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox() # Включаем браузер
-browser.get('http://localhost:8000') # Переходим по ссылке
 
-assert 'Django' in browser.title #  проверяем наличие Django  в тайтле
+class NewVisitorTest(unittest.TestCase):
+	'''new visitor test'''
+
+	def setUp(self):
+		'''instalation'''
+		self.browser = webdriver.Firefox() # Включаем браузер
+
+	def tearDown(self):
+		'''dismantling'''
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		'''тест: может начать и список и получить его позже'''
+		# Edit heard about a cool new online-application with
+		# urgent lists. She decides to estimate it's home page
+		self.browser.get('http://localhost:8000') # Переходим по ссылке
+
+		# SHe sees that title and header of the page say about urgent lists
+		self.assertIn('To-Do', self.browser.title) #  проверяем наличие To-DO  в тайтле
+		self.fail('Finish the test!')
+		
+		# She is immedeatly offered to enter list's element
+
+		# She types in the text area "Buy peacock feather"
+		# her hobby is knitting fishing flies
+
+		# When she pushes enter, the page is being updated. And now the page
+		# contains "1: Buy peacock feather"
+		# Edit is very methodical
+
+		# THe page is being updated again. And now it shows both elements of her list
+
+		# EDit wonders if the site remembers her list. Next she sees that
+		# the site generated for her unique YRL-address. A short text with explanations
+		# is displayed about this
+
+		# She visits this URL-address - her list is still there
+
+		# She is satisfied goes to sleep again
+		
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
