4f1363df0720aa3480d625b21190c8f97f1a2ef0
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-02-20 12:31:50 +0500

    Added lists app with unworking modul test

---
+++ lists/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+class SmokeTest(TestCase):
+	'''toxic test'''
+
+	def test_bad_maths(self):
+		'''тест'''
+
+		self.assertEqual(1+1, 3)
