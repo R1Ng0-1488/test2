ed6c39e933767dad0b609c70b669ca62683f9f55
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-02-23 13:25:02 +0500

    HTML of home page is generated from template now

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -44,7 +45,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock fether')
+			any(row.text == '1: Buy peacock fether' for row in rows),
+			"A new list's element didn't appeare in the table"
 		)
 		# Textarea is still invites her to add one more element
 		# She enters "Make fly out of peacock fethers"
--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.template.loader import render_to_string
 from django.http import HttpRequest
 
 from lists.views import home_page
@@ -9,16 +10,7 @@ from lists.views import home_page
 class HomePageTest(TestCase):
 	'''Тест домашней страницы'''
 
-	def test_root_url_resolves_to_home_page_view(self):
-		'''тест: корневой url преобразуется в представление домашней страницы'''
-		found = resolve('/') # через url находит функцию представления
-		self.assertEqual(found.func, home_page)
-
-	def test_home_page_returns_correct_html(self):
-		'''тест: домашняя страница возвращает правильный html'''
-		request = HttpRequest()
-		response = home_page(request)
-		html = response.content.decode('utf-8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
+	def test_uses_home_template(self):
+		'''тест: используется домашний шаблон'''
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
