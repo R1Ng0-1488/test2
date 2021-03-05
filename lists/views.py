8006624cc710b5840a94bd3aed4d4fdc62dc8bb8
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-03-05 13:01:14 +0500

    Added url to create a new list via POST

--- lists/views.py
+++ lists/views.py
@@ -6,12 +6,14 @@ from .models import Item
 
 def home_page(request):
 	'''home page'''
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/only_one_list_in_the_world/')
 	return render(request, 'home.html')
 
 def view_list(request):
 	'''представление списка'''
 	items = Item.objects.all()
 	return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+	'''новый список'''
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/only_one_list_in_the_world/')
