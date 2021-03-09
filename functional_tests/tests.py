bec0d6ec75e596d967014e12bc2788e81260500a
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-03-09 13:47:11 +0500

    First FT stemps for layout and styles

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -126,3 +126,28 @@ class NewVisitorTest(LiveServerTestCase):
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
+
+	def test_layout_adn_styling(self):
+		'''тест макета и стилевого оформления'''
+		# Edit opens home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# She pointes out that the input field is neatly centred
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		) 
+		# She begins a new list and sees that input field there
+		# is neasty centred too
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=10
+		)
