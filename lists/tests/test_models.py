8dda40cd635d50503a56d3a74c5ef9b0c545a2b2
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-04-07 12:07:17 +0500

    Implemented validation of the presence of a duplicate element on model level

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,41 +1,35 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
+from django.db.utils import IntegrityError
 
 from lists.models import Item, List
 # Create your tests here.
 
 
+class ItemModelTest(TestCase):
+	'''тест модели элемента'''
 
-class ListAndItemModelTest(TestCase):
-	'''Тест модели элемента списка'''
+	def test_default_text(self):
+		'''тест заданного по умолчанию текста'''
+		item = Item()
+		self.assertEqual(item.text, '')
 
-	def test_saving_and_retrieving_items(self):
-		'''тест сохранения и получения элементов'''
-		list_ = List()
-		list_.save()
+	def test_starting_representation(self):
+		'''тест строкового представления'''
+		item = Item(text='some text')
+		self.assertEqual(str(item), 'some text')
 
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.list = list_
-		first_item.save()
 
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.list = list_
-		second_item.save()
+class ListModelTest(TestCase):
+	'''Тест модели  списка'''
 
-		saved_list = List.objects.first()
-		self.assertEqual(saved_list, list_)
-
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
-
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-		self.assertEqual(first_saved_item.list, list_)
-		self.assertEqual(second_saved_item.text, 'Item the second')
-		self.assertEqual(second_saved_item.list, list_)
+	def test_item_is_related_to_list(self):
+		'''тест: элемент связан со списком'''
+		list_ = List.objects.create()
+		item = Item()
+		item.list = list_
+		item.save()
+		self.assertIn(item, list_.item_set.all())
 
 	def test_cannot_save_empty_list_items(self):
 		'''тест: нельзя добавлять пустые элементы списка'''
@@ -47,6 +41,33 @@ class ListAndItemModelTest(TestCase):
 
 	def test_get_absolute_url(self):
 		'''тест: получени абсолютный url'''
-
 		list_ = List.objects.create()
 		self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+
+	def test_duplicate_items_are_invalid(self):
+		'''тест: повторы элементов не допустимы'''
+		list_ = List.objects.create()
+		Item.objects.create(list=list_, text='bla')
+		with self.assertRaises(IntegrityError):
+			item = Item(list=list_, text='bla')
+			# item.full_clean()
+			item.save()
+
+	def test_CAN_save_sane_item_to_different_lists(self):
+		'''тест: МОЖЕТ сохранить тот же элемент в разные списки'''
+		list1 = List.objects.create()
+		list2 = List.objects.create()
+		Item.objects.create(list=list1, text='bla')
+		item = Item(list=list2, text='bla')
+		item.full_clean()
+
+	def test_list_ordering(self):
+		'''тест упорядочения списка'''
+		list1 = List.objects.create()
+		item1 = Item.objects.create(list=list1, text='i1')
+		item2 = Item.objects.create(list=list1, text='item 2')
+		item3 = Item.objects.create(list=list1, text='3')
+		self.assertEqual(
+			list(Item.objects.all()),
+			[item1, item2, item3]
+		)
