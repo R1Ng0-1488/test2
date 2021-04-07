2e415cbca0f82680af4e93679ca0750855bb9952
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-04-07 12:40:25 +0500

    x

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 
-from ..forms import ItemForm, EMPTY_ITEM_ERROR
+from ..forms import (
+	ItemForm, EMPTY_ITEM_ERROR,
+	DUPLICATION_ITEM_ERROR, ExistingListItemForm
+)
 from ..models import List, Item
 
 
@@ -30,3 +33,28 @@ class ItemFormTest(TestCase):
 		self.assertEqual(new_item, Item.objects.first())
 		self.assertEqual(new_item.text, 'do me')
 		self.assertEqual(new_item.list, list_)
+
+
+class ExistingListItemFormTest(TestCase):
+	'''Тест формы элементов существующего списка'''
+
+	def test_form_renders_item_text_input(self):
+		'''тест: форма отображает текстовый ввод элемента'''
+		list_ = List.objects.create()
+		form = ExistingListItemForm(for_list=list_)
+		self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+
+	def test_form_validation_for_blank_item(self):
+		'''тест: валидация формы для пустых элементов'''
+		list_ = List.objects.create()
+		form = ExistingListItemForm(for_list=list_, data={'text': ''})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
+	def test_form_validation_for_duplicate_items(self):
+		'''тест: валидация формы для повторных элементов'''
+		list_ = List.objects.create()
+		Item.objects.create(list=list_, text='no twins!')
+		form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
+		self.assertFalse(form.is_valid())
+		self.assertEqual(form.errors['text'], [DUPLICATION_ITEM_ERROR])
