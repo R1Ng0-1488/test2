f97cf0514d90d2cee636afdbd2b0cc3380d4e280
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-04-28 12:26:18 +0500

    some changes were done

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -8,6 +8,18 @@ import os
 
 MAX_WAIT = 10
 
+def wait(fn):
+	def method_fn(*args, **kwargs):
+		start_time = time.time()
+		while True:
+			try:
+				return fn(*args, **kwargs)
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
+	return method_fn 
+
 
 class FunctionalTest(StaticLiveServerTestCase):
 	'''new visitor test'''
@@ -26,48 +38,32 @@ class FunctionalTest(StaticLiveServerTestCase):
 		'''dismantling'''
 		self.browser.quit()
 
+	@wait
 	def wait_for(self, fn):
 		'''ожидание'''
-		start_time = time.time()
-		while True:
-			try:
-				return fn()
-			except (AssertionError, WebDriverException) as e:
-				if time.time() - start_time > MAX_WAIT:
-					raise e
-				time.sleep(0.5)
-
-
+		return fn()
+		
+	@wait
 	def wait_for_row_in_list_table(self, row_text):
 		'''ожидание строки в таблице списка'''
-		start_time = time.time()
-		while True:
-			try:
-				table = self.browser.find_element_by_id('id_list_table')
-				rows = table.find_elements_by_tag_name('tr')
-				self.assertIn(row_text, [row.text for row in rows])
-				return
-			except (AssertionError, WebDriverException) as e:
-				if time.time() - start_time > MAX_WAIT:
-					raise e
-				time.sleep(0.5)
-
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+			
 	def get_item_input_box(self):
 		'''получить поле ввода для элемента'''
 		return self.browser.find_element_by_id('id_text')
 
+	@wait
 	def wait_to_be_logged_in(self, email):
 		'''ожидать входа в систему'''
-		self.wait_for(
-			lambda: self.browser.find_element_by_link_text('Log Out')
-		)
+		self.browser.find_element_by_link_text('Log Out')
 		navbar = self.browser.find_element_by_css_selector('.navbar')
 		self.assertIn(email, navbar.text)
 
+	@wait
 	def wait_to_be_logged_out(self, email):
 		'''ожидать выхода из систему'''
-		self.wait_for(
-			lambda: self.browser.find_element_by_name('email')
-		)
+		self.browser.find_element_by_name('email')
 		navbar = self.browser.find_element_by_css_selector('.navbar')
 		self.assertNotIn(email, navbar.text)
