e1f6c56b2d8d1d0193f060b01dea3ad537e1761f
R1Ng0-1488 <57253219+R1Ng0-1488@users.noreply.github.com>
2021-05-10 12:34:12 +0500

    URL, sub view and template variables for my_lists

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -178,3 +178,12 @@ class NewListTest(TestCase):
 		'''тест на недопустимый ввод: форма передается в шаблон'''
 		response = self.client.post('/lists/new', data={'text': ''})
 		self.assertIsInstance(response.context['form'], ItemForm)
+
+
+class MyListsTest(TestCase):
+	'''тест моих списков'''
+
+	def test_my_lists_url_renders_my_lists_template(self):
+		'''тест: мои списки обрабатывают шаблон мои списков'''
+		response = self.client.get('/lists/users/a@b.com/')
+		self.assertTemplateUsed(response, 'my_lists.html')
